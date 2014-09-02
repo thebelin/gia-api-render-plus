7504572e6aa3b20aca1c949b1bce27e6dcb8435a
Belin Fieldson <thebelin@gmail.com>
2014-09-01 21:50:45 -0600

    Even more changes
    Too many to list

--- public/script/pageReady.js
+++ public/script/pageReady.js
@@ -7,6 +7,51 @@ $(document).ready(function () {
     // This tracks if the user is on a mobile browser
     isMobile = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase())),
 
+    // The event to track to determine click state
+    btnEvent = isMobile ? 'touchstart' : 'click',
+
+    // Use whatever function to display a modal
+    showModal = function (modalContents, onClose) {
+      // In this case, we're using a sliding drawer
+      
+      // put the contents in the drawer
+      $('.drawerContents').html(modalContents).show();
+
+      // Fade the page
+      $('#contentOverlay').show().animate({opacity:1}, 500);
+
+      // Slide the drawer
+      $('.drawer').animate({height: window.innerHeight * 0.99}, 500, function(){
+        // show the exit button and make it function
+        $('.exitDrawer').off().on(btnEvent, function() {
+          $('.exitDrawer').hide(300);
+          $('#contentOverlay').animate({opacity:0}, 300).hide();
+          $('.drawer').animate({height: '4em'}, 300);
+          if (onClose && typeof onClose === 'function') {
+            onClose.call(this);
+          }
+        }).show(500);
+      });
+    },
+
+    /**
+     * Render a social interaction page
+     * 
+     * @param String system The identifier for the social site
+     * 
+     * @return void
+     */
+    getSocial = function (system) {
+      console.log('render social:', system);
+      // Based on the system, do whatever the window calls for
+      switch (system) {
+        case 'facebook':
+          return $(".facebook.socialModule").html()
+          break;
+      }
+
+    },
+
     // Start the shoppingCart with all the options set
     cart = simpleCart({
       debug: true,
@@ -26,18 +71,12 @@ $(document).ready(function () {
 
       // event callbacks 
       beforeAdd               : null,
-      afterAdd                : function (data) {
-        console.log('cart add', data);
-      },
-      load                    : function (data) {
-        console.log('cart load', data);
-      },
+      afterAdd                : null,
+      load                    : null,
       beforeSave              : null,
       afterSave               : null,
       update                  : null,
-      ready                   : function (data) {
-        console.log('cart ready', data);
-      },
+      ready                   : null,
 
       // Checkout events
       checkoutSuccess         : null,
@@ -50,29 +89,119 @@ $(document).ready(function () {
       pollInterval: 10000,
       localDataDom: 'ImaginaryEats',
       memoField:    'menuHash',
-      mode:         'dev',
+      mode:         'live',
       apiUrl:       'https://script.google.com/macros/s/AKfycbyvb-2gd5IDPf42P2CIS1f8EVesZfPTMZJNCsLyAvDnEnbYdJhb/exec',
       devUrl:       'https://script.google.com/macros/s/AKfycbwqjwmqASwN0-g0p7dLx_fsqHVc2mpO-e3M00BY8oBB/dev',
+
+      // Callback function for apiClient button init which executes after render
       initButtons: function() {
-        if (cart) { // && typeof cart.add === 'function') {
-          console.log('initButtons');
-          $('.addButton').off().on('click', function() {
-            var item = $.extend(
-              {},
-              $(this).data(),
-              {quantity: $(this).parent().children('.item_quantity').val()
-            });
-            console.log('item data', item);
-            simpleCart.add(item);
+        // Init the social buttons
+        $('.socialButton').off().on(btnEvent, function () {
+          $('.socialButton').removeClass('socialSelected');
+          $(this).addClass('socialSelected');
+          showModal(getSocial($(this).data('system')), function() {
+            $('.socialButton').removeClass('socialSelected');
           });
-          //onclick = "simpleCart.add({name:'baby lion', price: 34.95,size:'tiny',thumb:'e.png'});"
+        });
+
+        // Init the close details button
+        $('.closeDetails').off().on(btnEvent, function () {
+          $('.details').hide(300);
+          $('.mapLink').show(500);
+          $('.mapLink').off().on(btnEvent, function () {
+            $('.details').show(300);
+            $('.mapLink').hide();
+          });
+        });
+
+        if (simpleCart && typeof simpleCart.add === 'function') {
+          // Attach the add button event to adding the item to the cart
+          $('.addButton').off().on(btnEvent, function() {
+            simpleCart.add($.extend({}, $(this).data(), {quantity: 1}));
+          });
+
+          // attach the add button click to the thumbnail
+          $('.itemThumb').off().on(btnEvent, function() {
+            console.log($(this).parent());
+            $(this).parent().parent().children('.addButton').trigger(btnEvent);
+          });
         } else {
-          console.log('no cart', cart);
+          // There was no cart to attach to
+          console.log('no cart', simpleCart);
         }
       },
 
-      onRender: function () {
+      // Callback function for apiClient events which executes after render
+      onRender: function() {
+        // A holder for the timer's reference
+        var previewTimer = null,
 
+        // Number the number of ms the preview should be active
+        previewTimeout = 5000,
+
+        // A function to hide the preview
+        hidePreview = function (callback) {
+          $('#previewOverlay').finish();
+          $('#preview').stop(true);
+          $('#previewOverlay').animate({opacity: 1}, 200, function () {
+            // change the preview to no image
+            $('#preview').css({opacity: 0, background: ''});
+            
+            // reveal the background
+            $('#previewOverlay').animate({opacity: 0}, 200, function(){
+              if (typeof callback === 'function') {
+                callback.call();
+              }
+            });
+          });
+        },
+
+        /**
+         * And one to do the preview
+         * @param String previewImg the url for the preview image
+         * 
+         * @return void
+         */
+        doPreview = function (previewImg, callback) {
+          if ($('#preview').css('background').indexOf(previewImg) === -1) {
+            // clear the timeout and set it again
+            if (previewTimer) {
+              // finish all existing animation sequences
+              $('#previewOverlay').finish().css({opacity:1});
+              clearTimeout(previewTimer);
+            }
+            previewTimer = setTimeout( function () {
+              previewTimer = null;
+              hidePreview();
+            }, previewTimeout);
+            
+            // cover the existing preview by making the mask opacity 1
+            $('#previewOverlay').animate({opacity: 1}, 200, function () {
+              // change the preview to the new image source from the data element
+              $('#preview').css({opacity: 1, 'background-image': 'url("' + previewImg + '")'});
+              
+              // display the preview
+              $('#previewOverlay').animate({opacity: 0}, 200, function(){
+                if (typeof callback === 'function') {
+                  callback.call();
+                }
+              });
+            });
+          } else {
+            // console.log($('#preview').css('background-image').indexOf(previewImg),$('#preview').css('background-image'), previewImg);
+          }
+        };
+
+        // Attach a preview to the items with preview image data
+        $('.menuItem[data-preview]').off().on('mouseover', function () {
+          doPreview($(this).data('preview'));
+        }).on(btnEvent, function () {
+          // Also attach to the interaction event
+          doPreview($(this).data('preview'));
+        });
+
+        // Do the resizer when the render runs
+        resizer();
       }
     }),
 
@@ -90,7 +219,12 @@ $(document).ready(function () {
         fontObj = {'font-size': Math.floor((window.innerWidth / zoomTo) * 100) / 100 + 'em'};
       }
       $('body').css(fontObj);
+      $(document.getElementById('content')).innerHeight(window.innerHeight * .9);
     };
+
+  window.onscroll = function () {
+    window.scrollTo(0, 0);
+  }
 
   window.onresize = function () {
     resizer();
